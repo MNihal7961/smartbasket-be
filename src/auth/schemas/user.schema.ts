01d0df9a0d05a678448f57673cd82d4eaa153588
“MNihal7961 <mnihal7961@gmail.com>
2025-07-09 12:16:17 +0530

    feat: add role property to User schema with enum validation

--- src/auth/schemas/user.schema.ts
+++ src/auth/schemas/user.schema.ts
@@ -22,6 +22,9 @@ export class User {
 
   @Prop({ required: true })
   lastName: string;
+
+  @Prop({ required: true, enum: ['user', 'admin'], default: 'user' })
+  role: 'user' | 'admin';
 }
 
 export const UserSchema = SchemaFactory.createForClass(User);
