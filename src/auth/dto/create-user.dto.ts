f426732c3bdd28dc7d5839ce9604efb9589bed95
“MNihal7961 <mnihal7961@gmail.com>
2025-07-09 13:05:27 +0530

    refactor: add ApiProperty decorators to CreateUserDto and SignInUserDto for improved Swagger documentation

--- src/auth/dto/create-user.dto.ts
+++ src/auth/dto/create-user.dto.ts
@@ -1,7 +1,18 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class CreateUserDto {
+  @ApiProperty()
   firstName: string;
+
+  @ApiProperty()
   lastName: string;
+
+  @ApiProperty()
   email: string;
+
+  @ApiProperty()
   password: string;
+
+  @ApiProperty({ enum: ['user', 'admin'], required: false })
   role?: 'user' | 'admin';
 }
--- src/auth/dto/signin-user.dto.ts
+++ src/auth/dto/signin-user.dto.ts
@@ -1,4 +1,9 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class SignInUserDto {
+  @ApiProperty()
   email: string;
+
+  @ApiProperty()
   password: string;
 } 
